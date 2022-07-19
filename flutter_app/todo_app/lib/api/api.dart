6caee8004afe8a25fc9f58feeeff832a761da29e
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-19 13:51:43 +0430

    create maop

--- flutter_app/todo_app/lib/api/api.dart
+++ flutter_app/todo_app/lib/api/api.dart
@@ -2,3 +2,16 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../models/todo_models.dart';
 import 'package:http/http.dart' as http;
+
+class TodoProvider extends ChangeNotifier {
+  List<Todo> _todos = [];
+
+  List<Todo> get todos {
+    return [..._todos];
+  }
+
+  fetchTasks() async {
+    final url = "http://127.0.0.1:8000/apis/v1/?format=json";
+    final response = await http.get(Uri.parse(url));
+  }
+}
